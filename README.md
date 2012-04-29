7b2ae73d8de57a8c7e7cbb29330a17cf97a02a59
Andrew Keig <andrew.keig@gmail.com>
2012-04-30 00:58:49 +0100

    0.0.1

--- README.md
+++ README.md
@@ -1,4 +1,63 @@
-dust-down
-=========
+# dust-down - compiling dust.js templates
 
-Compiles all dust.js template views in a given folder
+dust-down is a node.js command line tool which will compile all dust.js templates in a given directory
+
+we are currently supporting the following features:
+
+- versioning
+- nested folder structures
+
+
+# Install
+
+    $ npm install dust-down
+
+
+# Example 1; with path; default version number
+
+
+given the following folder structure
+
+    /path_to_templates/
+        template_one.html
+        template_two.html
+            /path_to_more_templates/
+                    template_three.html
+
+when we execute the compiler with a single path argument
+
+    node compile.js /path_to_templates/
+
+then dust-down will compile templates to
+
+    /path_to_templates/compiled_0_0_1
+        template_one_0_0_1.js
+            template_two_0_0_1.js
+                /path_to_more_templates/
+                        template_three_0_0_1.js
+
+
+
+# Example 2; with path and version number
+
+
+given the following folder structure
+
+    /path_to_templates/
+        template_one.html
+        template_two.html
+            /path_to_more_templates/
+                    template_three.html
+
+when we execute the compiler with a path and version argument
+
+    node compile.js /path_to_templates/ 0_0_5
+
+
+then dust-down will compile templates to
+
+    /path_to_templates/compiled_0_0_5
+        template_one_0_0_5.js
+            template_two_0_0_5.js
+                /path_to_more_templates/
+                        template_three_0_0_5.js
---
+++ compile.js
@@ -0,0 +1,62 @@
+var fs = require('fs');
+var walk = require('walkdir');
+var mkdirp = require('mkdirp');
+
+//get dust mobile; fallback to linkedin version
+try {
+   var dust = require('dust');
+} catch (err) {
+   var dust = require('dustjs-linkedin');
+}
+
+//ensure path arg is provided
+if (!process.argv[2]) {
+    console.error('Please provide a path to your dust views');
+    return;
+}
+
+var version = "0_0_1";
+var view_path = process.argv[2];
+
+//version number should be in the following format - 0_0_0
+if (process.argv[3]) version = process.argv[3].replace(/\./g,'_').replace(/\-/g,'_');
+
+//compile views to the following folder - path/compiled_0_0_0
+var compiled_folder = view_path + "/compiled_" + version + "/";
+
+mkdirp(compiled_folder, function (err) {
+    if (err) console.error(err)
+    else console.log('Created folder : ' + compiled_folder);
+});
+
+//walk through all files in path provided
+walk.sync(view_path, function(filepath, stat) {
+    if (stat.isDirectory() || filepath.indexOf("compiled") != -1) return;
+
+    fs.readFile(filepath, 'ascii', function (err, data) {
+        if (err) console.error(err);
+        var path_parts = filepath.split("/");
+        var file_to_create = path_parts[0] + "\\compiled_" + version;
+        path_parts.splice(0, 1);
+
+        for (folder in path_parts) {
+            if (path_parts[folder].indexOf("html") == -1) {
+                file_to_create = file_to_create + "\\" + path_parts[folder];
+
+                mkdirp(file_to_create, function (err) {
+                    if (err) console.error(err)
+                    console.log('Created folder : ' + file_to_create);
+                });
+            }
+        }
+
+        var filename = filepath.split("/").reverse()[0].replace(".html", "");
+        file_to_create = file_to_create + "\\" + filename + "_"  + version + ".js";
+        var compiled = dust.compile(data, filename);
+
+        fs.writeFile(file_to_create, compiled, function (err) {
+            if (err) console.error(err);
+            console.log('Dust view compiled: ' + file_to_create);
+        });
+    });
+});
